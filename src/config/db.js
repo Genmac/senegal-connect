debeb82bc35972b7a691975f75d11f6b8ffa6fd4
Macoumba <elhadjimacoumbandiaye62@gmail.com>
2026-08-03 11:45:29 -0400

    feat: configuration de l'application et pool postgresql

---
+++ src/config/db.js
@@ -0,0 +1,22 @@
+// src/config/db.js
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env')});
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  getTransactionClient: async () => {
+    const client = await pool.connect();
+    return client;
+  },
+  pool,
+};
